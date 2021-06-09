bea475dcdc2cd17428b84c977dfa22445b7c7067
Liyajoseph <84517633+Liyajoseph@users.noreply.github.com>
2021-06-09 15:18:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('button').addEventListener('click', check)
+let age = 0
+let date = 'Saturday'
+
+function check () {
+  age = document.getElementById('input1').value
+  age = parseInt(age)
+
+  date = document.getElementById('input2').value
+
+  if ((date === 'Wednesday' || date === 'Saturday') || (age > 13 || age < 20)){ 
+    document.getElementById('answer').innerHTML = 'You get student pricing.'
+      } else {
+    document.getElementById('answer').innerHTML = 'You get regular pricing.'
+  }
+}
