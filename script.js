241ac18ccc8936b5840eb5f409da63283f5435d8
Liyajoseph <84517633+Liyajoseph@users.noreply.github.com>
2021-06-09 15:37:53 +0000

    document.getElementById('button').addEventListener('click', check) let age = 0 let date = 'Saturday'
    function check () {
      age = document.getElementById('input2').value
      age = parseInt(age)
    
      date = document.getElementById('input1').value
    
      if ((date === 'Wednesday' || date === 'Saturday') || (age > 12 && age < 21)) {
        document.getElementById('answer').innerHTML = 'You get student pricing.'
      } else {
        document.getElementById('answer').innerHTML = 'You get regular pricing.'
      }
    }

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ let age = 0
 let date = 'Saturday'
 
 function check () {
-  age = document.getElementById('input1').value
+  age = document.getElementById('input2').value
   age = parseInt(age)
 
-  date = document.getElementById('input2').value
+  date = document.getElementById('input1').value
 
-  if ((date === 'Wednesday' || date === 'Saturday')
-  (age > 13 || age < 20)) { 
+  if ((date === 'Wednesday' || date === 'Saturday') || (age > 12 && age < 21)) {
     document.getElementById('answer').innerHTML = 'You get student pricing.'
   } else {
     document.getElementById('answer').innerHTML = 'You get regular pricing.'
